af0045a4e1884d67aa0b44c8629125201f676d7b
payalsisodiya55 <payalsisodiya038@gmail.com>
2026-03-30 16:26:15 +0530

    bulk edit done

--- frontend/src/modules/admin/pages/AdminStockManagement.tsx
+++ frontend/src/modules/admin/pages/AdminStockManagement.tsx
@@ -1383,6 +1383,7 @@ export default function AdminStockManagement() {
                       <option value={20}>20</option>
                       <option value={50}>50</option>
                       <option value={100}>100</option>
+                      <option value={500}>500</option>
                     </select>
                   </div>
 
--- frontend/src/modules/seller/pages/SellerStockBulkEdit.tsx
+++ frontend/src/modules/seller/pages/SellerStockBulkEdit.tsx
@@ -1685,6 +1685,7 @@ export default function SellerStockBulkEdit({
                 <option value={20}>20</option>
                 <option value={50}>50</option>
                 <option value={100}>100</option>
+                <option value={500}>500</option>
               </select>
             </div>
 
